be9821162feba2e8edb2fe80aa579c9b5ae89b9e
almubarakwaqf <almubarakwf@gmail.com>
2024-07-05 10:33:25 +0100

    home component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+    <h1>Hello world!</h1>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = 'homes';
 }
